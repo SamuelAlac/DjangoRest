1c7f98f43f81d1853da9222ed4a88e8ca59dff50
SamuelAlac <samuelalac21@gmail.com>
2025-10-18 06:14:58 +0800

    app with database

---
+++ studybud/base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'base'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('room/<str:pk>/', views.room_view, name='room'),
+]
